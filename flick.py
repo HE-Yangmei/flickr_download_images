ab26360a05b67e6a54b0cde1b02a27d65cd32c84
HE-Yangmei <heyangmei123@outlook.com>
2019-03-12 17:03:03 +0800

    Update flick.py

--- flick.py
+++ flick.py
@@ -5,7 +5,7 @@ import os
 # Change the follwing variables to match your own needs
 project_path = 'watercolor/'
 photos_per_tag = 500
-filenames = ['watercolor_flower.txt', 'cat.txt']
+filenames = ['wf.txt', 'cat.txt']
 
 
 def download_files(flickr, t, category, num_photos):
