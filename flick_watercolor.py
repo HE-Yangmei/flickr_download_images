b1f5fa942251e8e4432df9da17740a833ad31e3b
HE-Yangmei <heyangmei123@outlook.com>
2019-03-14 16:49:26 +0800

    Update flick_watercolor.py

--- flick_watercolor.py
+++ flick_watercolor.py
@@ -3,7 +3,7 @@ import urllib.request
 import os
 
 # Change the follwing variables to match your own needs
-project_path = 'watercolor/'
+project_path = 'flickr_download_images/'
 photos_per_tag = 500
 filenames = ['watercolor_flower.txt', 'cat.txt']
 
