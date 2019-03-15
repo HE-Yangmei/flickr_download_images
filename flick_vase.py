ea2e84a34be7c35eb3da4b18190207eb4f15c74c
HE-Yangmei <heyangmei123@outlook.com>
2019-03-15 20:07:34 +0800

    Update and rename flick_flower.py to flick_vase.py

--- flick_vase.py
+++ flick_vase.py
@@ -3,9 +3,9 @@ import urllib.request
 import os
 
 # Change the follwing variables to match your own needs
-project_path = 'flickr_download_images/'
-photos_per_tag = 1000
-filenames = ['flower.txt', 'watercolor_flower.txt']
+project_path = 'vase/'
+photos_per_tag = 500
+filenames = ['vase.txt', 'cat.txt']
 
 
 def download_files(flickr, t, category, num_photos):
